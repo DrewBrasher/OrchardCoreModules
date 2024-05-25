7262a44249e0c9e43eed2c2f4fcf112237525bbb
Drew Brasher <abrasher79@gmail.com>
2024-05-24 19:29:55 -0500

    Updated Content Warning Manifest

--- DrewBrasher.OrchardCore.ContentWarning/Manifest.cs
+++ DrewBrasher.OrchardCore.ContentWarning/Manifest.cs
@@ -1,10 +1,10 @@
 using OrchardCore.Modules.Manifest;
 
 [assembly: Module(
-    Name = "DrewBrasher.OrchardCore.ContentWarning",
-    Author = "The Orchard Core Team",
-    Website = "https://orchardcore.net",
-    Version = "0.0.1",
-    Description = "DrewBrasher.OrchardCore.ContentWarning",
+    Name = "Content Warning",
+    Author = "Drew Brasher",
+    Website = "https://github.com/DrewBrasher/OrchardCoreModules",
+    Version = "0.1.0",
+    Description = "An Orchard Core CMS Module that allows you to put a content warning around some content so that users can decide if they want to see it or not.",
     Category = "Content Management"
 )]
