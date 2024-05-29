215e325ec7aa0b91edb6ae158a363f421c4f9fd0
Drew Brasher <brasherd@pbjcal.org>
2024-05-29 07:02:46 -0500

    Removed Comments

--- DrewBrasher.OrchardCore.ContentWarning/Drivers/ContentWarningContentDisplayDriver.cs
+++ DrewBrasher.OrchardCore.ContentWarning/Drivers/ContentWarningContentDisplayDriver.cs
@@ -16,16 +16,13 @@ internal class ContentWarningContentDisplayDriver : ContentDisplayDriver
     public override Task<IDisplayResult> DisplayAsync(ContentItem model, BuildDisplayContext context)
     {
         var shape = context.Shape;
-        // If the content item contains FormPart add Form Wrapper only in Display type Detail
         var part = model.As<ContentWarningPart>();
         if(part != null && part.ShowWarning)
         {
-            // Add wrapper for content type if template is not available it will fall back to Form_Wrapper
             shape.Metadata.Wrappers.Add($"ContentWarning_Wrapper__{model.ContentType}");
             shape.Metadata.Wrappers.Add($"ContentWarning_Wrapper");
         }
 
-        // We don't need to return a shape result
         return Task.FromResult<IDisplayResult>(null);
     }
 }
