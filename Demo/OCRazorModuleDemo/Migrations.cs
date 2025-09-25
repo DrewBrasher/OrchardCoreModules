5a40791310a3332786ec4fddaa425c8d30551e6b
Drew Brasher <abrasher79@gmail.com>
2025-09-25 09:39:43 -0500

    Added an example of using a Blazor Component inside a Widget.

---
+++ Demo/OCRazorModuleDemo/Migrations.cs
@@ -0,0 +1,33 @@
+﻿using OrchardCore.ContentManagement.Metadata;
+using OrchardCore.ContentManagement.Metadata.Settings;
+using OrchardCore.Data.Migration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OCRazorModuleDemo;
+public class Migrations : DataMigration
+{
+    private readonly IContentDefinitionManager _contentDefinitionManager;
+
+    public Migrations(IContentDefinitionManager contentDefinitionManager) =>
+        _contentDefinitionManager = contentDefinitionManager;
+
+    public async Task<int> CreateAsync()
+    {
+        await _contentDefinitionManager.AlterTypeDefinitionAsync("DemoWidget", type => type
+            .DisplayedAs("Demo Widget")
+            .Creatable()
+            .Listable()
+            .Draftable()
+            .Versionable()
+            .Securable()
+            .Stereotype("Widget")
+            .WithPart("DemoWidget")
+        );
+
+        return 1;
+    }
+}
--- Demo/OCRazorModuleDemo/Startup.cs
+++ Demo/OCRazorModuleDemo/Startup.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Routing;
 using Microsoft.Extensions.DependencyInjection;
+using OrchardCore.Data.Migration;
 using OrchardCore.Modules;
 
 namespace OCRazorModuleDemo;
@@ -9,6 +10,7 @@ public sealed class Startup : StartupBase
 {
     public override void ConfigureServices(IServiceCollection services)
     {
+        services.AddDataMigration<Migrations>();
     }
 
     public override void Configure(IApplicationBuilder builder, IEndpointRouteBuilder routes,
